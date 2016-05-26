0cd42d5d73bab5ed2d7a07e232f7ce7c6332b9c8
Dan Jones <androidftw1994@gmail.com>
2016-05-27 00:47:43 +0100

    Finished version 1

---
+++ Test/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using confluence.slackbot;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            notifier.config cfg = new notifier.config();
+
+            cfg.channel = "HELIX";
+            cfg.team = "confluence-software";
+            cfg.token = "[REDACTED]";
+
+            Console.WriteLine("Slackbot notifier test app");
+            Console.WriteLine();
+
+            Console.Write("Enter message: ");
+            string message = Console.ReadLine();
+            bool status = notifier.send(cfg, message);
+
+            Console.WriteLine();
+            Console.WriteLine("STATUS: " + status.ToString());
+            Console.ReadLine();
+        }
+    }
+}
--- slackbotNotifier/Class1.cs
+++ slackbotNotifier/Class1.cs
@@ -1,12 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace slackbotNotifier
+namespace confluence.slackbot
 {
-    public class Class1
+    public class notifier
     {
+        public class config
+        {
+            public string team;
+            public string token;
+            public string channel;
+        }
+        public static bool send(config cfg, string message)
+        {
+            bool result = false;
+
+            try
+            {
+                WebRequest request = WebRequest.Create(@"https://" + cfg.team + @".slack.com/services/hooks/slackbot?channel=" + cfg.channel + @"&token=" + cfg.token);
+                request.Method = "POST";
+                string postData = message.ToString();
+                byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.ContentLength = byteArray.Length;
+                Stream dataStream = request.GetRequestStream();
+                dataStream.Write(byteArray, 0, byteArray.Length);
+                dataStream.Close();
+                WebResponse response = request.GetResponse();
+                dataStream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(dataStream);
+                string responseFromServer = reader.ReadToEnd();
+
+                if (responseFromServer == "ok")
+                {
+                    result = true;
+                }
+
+                reader.Close();
+                dataStream.Close();
+                response.Close();
+            }
+            catch(Exception)
+            {
+                result = false;
+            }
+
+            return result;
+        }
     }
 }
